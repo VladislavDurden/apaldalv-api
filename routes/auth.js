7e1f8277255263548d53c00d67d74454f7c793cb
Vladislav Petrovskiy <vpetrovskiy@tula.co>
2020-05-23 05:28:21 +0300

    minor revert

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const {MongoClient} = require('mongodb');
-const uri = `mongodb+srv://admin:apaldalv!_@cluster0-1qm9j.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://admin:apaldalv!_@cluster0-1qm9j.mongodb.net/test?retryWrites=true&w=majority&useUnifiedTopology=true`;
 const client = new MongoClient(uri);
 const DB_NAME = 'apaldalv';
 
